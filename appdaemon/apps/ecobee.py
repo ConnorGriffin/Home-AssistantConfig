9ff42010246f08d6bedc48694d6c72beb32957f1
Connor Griffin <connor@connorcg.com>
2018-06-25 20:16:19 -0700

    Simplified scheduling with 'immediate = True' and delay Also moved to per-callback constraints using a config variable

--- appdaemon/apps/ecobee.py
+++ appdaemon/apps/ecobee.py
@@ -9,130 +9,113 @@ class Ecobee(hass.Hass):
             'home_status': None
         }
 
-        ecobee_entity = self.args['ecobee_entity']
-        presence_entity = self.args['presence_entity']
-        
+        constrain_input_boolean = self.args['constrain_boolean']
+
         for state in ['home', 'not_home']:
-            # Set home_status global var on init, then setup listeners to keep it updated
-            duration = self.args['{}_delay'.format(state)]
-            self.home_status_cb(dict(
-                entity_id = presence_entity,
-                state = state,
-                state_duration = duration
-            ))
+            # Setup a listener for each home state with custom durations - 'immediate = True' makes these eval on first load
             self.listen_state(
                 self.presence_cb,
-                entity_id = self.args['presence_entity'],
+                entity = self.args['presence_entity'],
                 new = state,
-                state_duration = duration
+                immediate = True,
+                duration = self.args['{}_delay'.format(state)],
+                constrain_input_boolean = constrain_input_boolean
             )
-
+    
         # Listen for any ecobee state changes
         self.listen_state(
             self.ecobee_state_cb,
-            entity = ecobee_entity,
+            entity = self.args['ecobee_entity'],
+            constrain_input_boolean = constrain_input_boolean
         )
         
         # Re-check state when the automation is re-enabled
         self.listen_state(
-            self.ecobee_state_cb,
-            entity_id = self.args['constraint_input_boolean'],
-            new = 'on'
+            self.constrain_boolean_cb,
+            entity = constrain_input_boolean
         )
 
-    
-    def ecobee_action_cb(self):
-        if (self.get_state(self.args['constraint_input_boolean']) == 'on'):
-            home_modes = self.args.get('climate_modes', {}).get('usually_home',[])
-            away_modes = self.args.get('climate_modes', {}).get('usuaully_not_home',[])
-            ecobee_entity = self.args['ecobee_entity']
-            state_data = self.get_state(ecobee_entity, attribute='all')
-            home = self.global_vars['ecobee']['home_status'] == 'home'
-            away_hold = state_data['attributes']['away_mode']
-            hold_mode = state_data['attributes']['hold_mode']
-            climate_mode = state_data['attributes']['climate_mode']
 
-            # For some reason away holds set away_hold and not hold_mode, handle this
-            if not hold_mode and away_hold == "on":
-                hold_mode = "away"
-            
-            # This is so we don't cancel temp or vacation holds
+    # Evaluates ecobee state and determines a hold/resume action
+    def ecobee_actions(self):
+        # Get config data
+        home_modes = self.args.get('climate_modes', {}).get('usually_home',[])
+        away_modes = self.args.get('climate_modes', {}).get('usuaully_not_home',[])
+        ecobee_entity = self.args['ecobee_entity']
+        home = self.global_vars['ecobee']['home_status'] == 'home'
+        
+        # Get ecobee state data
+        state_data = self.get_state(ecobee_entity, attribute='all')
+        away_hold = state_data['attributes']['away_mode']
+        hold_mode = state_data['attributes']['hold_mode']
+        climate_mode = state_data['attributes']['climate_mode']
+
+        # For some reason away holds set away_hold and not hold_mode, handle this
+        if not hold_mode and away_hold == "on":
+            hold_mode = "away"
+        
+        # This is so we don't cancel temp or vacation holds
+        if home:
+            hold_match = hold_mode in ['away', 'home'] or not hold_mode
+        else:
+            hold_match = hold_mode in ['away', 'home', 'temp'] or not hold_mode
+
+        # Don't run if the ecobee is off
+        if hold_match and state_data['attributes']['operation_mode'] != 'off':
             if home:
-                hold_match = hold_mode in ['away', 'home'] or not hold_mode
-            else:
-                hold_match = hold_mode in ['away', 'home', 'temp'] or not hold_mode
-
-            # Don't run if the ecobee is off
-            if hold_match and state_data['attributes']['operation_mode'] != 'off':
-                if home:
-                    if hold_mode != 'home' and climate_mode in away_modes:
-                        # Home during a standard away time, set a home hold
-                        self.log('Setting a home hold.')
-                        self.call_service(
-                            service = 'climate/set_hold_mode',
-                            entity_id = ecobee_entity,
-                            hold_mode = 'home'
-                        )
-                    elif hold_mode and climate_mode in home_modes:
-                        # Home during a hold and comfort setting is a home setting
-                        self.log('Resuming action (home).')
-                        self.call_service(
-                            service = 'climate/ecobee_resume_program',
-                            entity_id = ecobee_entity,
-                            resume_all = True
-                        )
-                elif not home:
-                    if hold_mode != 'away' and climate_mode in home_modes:
-                        # Comfort mode isn't away and we aren't in a home hold
-                        self.log('Setting an away hold.')
-                        self.call_service(
-                            service = 'climate/set_hold_mode',
-                            entity_id = ecobee_entity,
-                            hold_mode = 'away'
-                        )
-                    elif hold_mode and climate_mode in away_modes:
-                        # Away during a hold and comfort setting is an away setting
-                        self.log('Resuming action (away).')
-                        self.call_service(
-                            service = 'climate/ecobee_resume_program',
-                            entity_id = ecobee_entity,
-                            resume_all = True
-                        )
-
-
-    # Callback for ecobee state changes
-    def ecobee_state_cb(self, entity, attribute, old, new, kwargs):
-        self.ecobee_action_cb()
-
-
-    # Check if state has existed for an arbitrary duration in seconds
-    def home_status_cb(self, kwargs):
-        # Can't use named parameters because I'm using this as a callback
-        entity = kwargs['entity_id']
-        state = kwargs['state']
-        duration = kwargs['state_duration']
-
-        now = datetime.utcnow()
-        state_data = self.get_state(entity, attribute='all')
-        #self.log(state_data)
-        last_changed = self.convert_utc(state_data['last_changed'])
-        last_changed = last_changed.replace(tzinfo=None)
-        duration = (now - last_changed).total_seconds() >= duration
-
-         # Update global vars home_status if state has matched state argument for duration number of seconds
-        if duration and state_data['state'] == state:
-            self.global_vars['ecobee']['home_status'] = state
-            self.ecobee_action_cb()
-
-
-    # Callback for presence changes
+                if hold_mode != 'home' and climate_mode in away_modes:
+                    # Home during a standard away time, set a home hold
+                    self.log('Setting a home hold.')
+                    self.call_service(
+                        service = 'climate/set_hold_mode',
+                        entity_id = ecobee_entity,
+                        hold_mode = 'home'
+                    )
+                elif hold_mode and climate_mode in home_modes:
+                    # Home during a hold and comfort setting is a home setting
+                    self.log('Resuming action (home).')
+                    self.call_service(
+                        service = 'climate/ecobee_resume_program',
+                        entity_id = ecobee_entity,
+                        resume_all = True
+                    )
+            elif not home:
+                if hold_mode != 'away' and climate_mode in home_modes:
+                    # Comfort mode isn't away and we aren't in a home hold
+                    self.log('Setting an away hold.')
+                    self.call_service(
+                        service = 'climate/set_hold_mode',
+                        entity_id = ecobee_entity,
+                        hold_mode = 'away'
+                    )
+                elif hold_mode and climate_mode in away_modes:
+                    # Away during a hold and comfort setting is an away setting
+                    self.log('Resuming action (away).')
+                    self.call_service(
+                        service = 'climate/ecobee_resume_program',
+                        entity_id = ecobee_entity,
+                        resume_all = True
+                    )
+                    
+                    
+    # Update global variable and evaluate hold actions on presence change
     def presence_cb(self, entity, attribute, old, new, kwargs):
-        duration = kwargs.get('state_duration', 0)
-        # Check if the state is still the same after duration (number of seconds)
-        self.run_in(
-            self.home_status_cb,
-            seconds = duration+1,
-            entity_id = entity,
-            state = new,
-            state_duration = duration
-        )
+        self.global_vars['ecobee']['home_status'] = new
+        self.ecobee_actions()
+
+    # Evaluate hold action when boolean is turned on, set a resume all when it's switched off
+    def constrain_boolean_cb(self, entity, attribute, old, new, kwargs):
+        if new == 'on':
+            self.ecobee_actions()
+        elif new == 'off':
+            self.log('Resuming action (disabled).')
+            self.call_service(
+                service = 'climate/ecobee_resume_program',
+                entity_id = self.args['ecobee_entity'],
+                resume_all = True
+            )
+
+
+    # When ecobee state changes, re-evaluate hold actions
+    def ecobee_state_cb(self, entity, attribute, old, new, kwargs):
+        self.ecobee_actions()
