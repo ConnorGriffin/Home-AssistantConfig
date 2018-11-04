cc538d7ab4f263007f3ae8f3fe1d3c353132285f
Connor Griffin <connor@connorcg.com>
2018-11-04 00:07:15 -0700

    Added climate control logic, complete until I get more sensors

--- appdaemon/apps/door_window_climate.py
+++ appdaemon/apps/door_window_climate.py
@@ -4,17 +4,29 @@ import datetime
 class DoorWindowClimate(hass.Hass):
 
     def initialize(self):
-        # TODO: Create actions based on climate rules
-        # TODO: Handle temperature deltas, climate control, rest of the script basically
+        rules = self.args.get('climate_rules', [])
 
-        self.data = {}
+        # We'll put data in these later
+        self.zone_data = {}
+        self.rule_data = {}
+        self.rule_counts = {
+            'active_rules': [],
+            'inactive_rules': [],
+            'total_rules': len(rules)
+        }
+        self.climate_data = {
+            'last_mode': None,
+            'current_mode': self.get_state(self.args.get('climate'), attribute='operation_mode')
+        }
 
+        # Determine if each zone is open or closed based on the sensors in the zone
         for zone in self.args.get('zones', []):
             windows = zone.get('windows', [])
             doors = zone.get('doors', [])
-            temp_source = zone.get('temp_source', self.args.get('temp_source')).split(':')
-            temp_entity = temp_source[0]
-            temp_attribute = temp_source[1]
+
+            #temp_source = zone.get('temp_source', self.args.get('temp_source')).split(':')
+            #temp_entity = temp_source[0]
+            #temp_attribute = temp_source[1]
 
             # Create each zone sensor in HA
             self.set_state(
@@ -26,7 +38,7 @@ class DoorWindowClimate(hass.Hass):
             )
 
             # Setup a persistent data store for each zone
-            self.data[zone['name']] = {
+            self.zone_data[zone['name']] = {
                 'state': None,
                 'open_sensors': [],
                 'closed_sensors': [],
@@ -85,6 +97,80 @@ class DoorWindowClimate(hass.Hass):
                     sensor = zone['sensor']
                 )
 
+        # Figure out if each rule is open or closed based on a combination of zones and sensors
+        for rule in rules:
+            dependencies = rule.get('dependencies', [])
+
+            # Pull in the settings for each rule, otherwise use the default settings
+            door_open_seconds = rule.get('door_open_seconds', self.args['door_open_seconds'])
+            door_closed_seconds = rule.get('door_closed_seconds', self.args['door_closed_seconds'])
+            window_open_seconds = rule.get('window_open_seconds', self.args['window_open_seconds'])
+            window_closed_seconds = rule.get('window_closed_seconds', self.args['window_closed_seconds'])
+
+            # Setup a persistent data store for each rule
+            self.rule_data[rule['name']] = {
+                'state': None,
+                'open_sensors': [],
+                'closed_sensors': [],
+                'total_sensors': len(dependencies)
+            }
+
+            # Determine the dependency type and setup listeners
+            for dependency in dependencies:
+                if dependency.get('zone'):
+                    entity = dependency['zone']
+
+                    # Get the initial state (so stuff works if AppDaemon reboots)
+                    self.run_in(
+                        self.eval_rule,
+                        seconds = 5,
+                        rule = rule,
+                        entity_id = entity
+                    )
+
+                    # Listen for state changes
+                    self.listen_state(
+                        cb = self.dependency_cb,
+                        entity = entity,
+                        rule = rule
+                    )
+                elif dependency.get('door'):
+                    entity = dependency['door']
+                    self.listen_state(
+                        cb = self.dependency_cb,
+                        entity = entity,
+                        new = 'Closed',
+                        duration = door_closed_seconds,
+                        immediate = True,
+                        rule = rule
+                    )
+                    self.listen_state(
+                        cb = self.dependency_cb,
+                        entity = entity,
+                        new = 'Open',
+                        duration = door_closed_seconds,
+                        immediate = True,
+                        rule = rule
+                    )
+                elif dependency.get('window'):
+                    entity = dependency['window']
+                    self.listen_state(
+                        cb = self.dependency_cb,
+                        entity = entity,
+                        new = 'Closed',
+                        duration = window_closed_seconds,
+                        immediate = True,
+                        rule = rule
+                    )
+                    self.listen_state(
+                        cb = self.dependency_cb,
+                        entity = entity,
+                        new = 'Open',
+                        duration = window_closed_seconds,
+                        immediate = True,
+                        rule = rule
+                    )
+
 
     def sensor_cb(self, entity, attribute, old, new, kwargs):
         zone = kwargs.get('zone')
@@ -93,36 +179,139 @@ class DoorWindowClimate(hass.Hass):
 
         # Add or remove the sensor from the open_sensors and closed_sensors lists
         if new == 'Closed':
-            self.data[zone]['closed_sensors'].append(entity)
-            if entity in self.data[zone]['open_sensors']:
-                self.data[zone]['open_sensors'].remove(entity)
+            self.zone_data[zone]['closed_sensors'].append(entity)
+            if entity in self.zone_data[zone]['open_sensors']:
+                self.zone_data[zone]['open_sensors'].remove(entity)
         elif new == 'Open':
-            self.data[zone]['open_sensors'].append(entity)
-            if entity in self.data[zone]['closed_sensors']:
-                self.data[zone]['closed_sensors'].remove(entity)
+            self.zone_data[zone]['open_sensors'].append(entity)
+            if entity in self.zone_data[zone]['closed_sensors']:
+                self.zone_data[zone]['closed_sensors'].remove(entity)
 
         # Determine if the zone should be considered opened or closed based on mode and sensor counts
-        open_count = len(self.data[zone]['open_sensors'])
-        closed_count = len(self.data[zone]['closed_sensors'])
+        open_count = len(self.zone_data[zone]['open_sensors'])
+        closed_count = len(self.zone_data[zone]['closed_sensors'])
 
         # Set zone state once all sensors are accounted for
-        if (open_count + closed_count) == self.data[zone]['total_sensors']:
+        if (open_count + closed_count) == self.zone_data[zone]['total_sensors']:
             if mode == 'any':
                 if open_count >= 1:
-                    # Set the Zone Status in HomeAssistant. Other AppDaemon apps can subscribe to this event to take action on the alarm
-                    self.log('{} is open'.format(zone))
-                    self.data[zone]['state'] = 'open'
+                    self.log('{} (zone) is open'.format(zone))
+                    self.zone_data[zone]['state'] = 'open'
                     self.set_state(sensor, state='Open', friendly_name=zone)
                 else:
-                    self.log('{} is closed'.format(zone))
-                    self.data[zone]['state'] = 'closed'
+                    self.log('{} (zone) is closed'.format(zone))
+                    self.zone_data[zone]['state'] = 'closed'
                     self.set_state(sensor, state='Closed', friendly_name=zone)
             elif mode == 'all':
                 if closed_count >= 1:
-                    self.log('{} is closed'.format(zone))
-                    self.data[zone]['state'] = 'closed'
+                    self.log('{} (zone) is closed'.format(zone))
+                    self.zone_data[zone]['state'] = 'closed'
                     self.set_state(sensor, state='Closed', friendly_name=zone)
                 else:
-                    self.log('{} is open'.format(zone))
-                    self.data[zone]['state'] = 'open'
+                    self.log('{} (zone) is open'.format(zone))
+                    self.zone_data[zone]['state'] = 'open'
                     self.set_state(sensor, state='Open', friendly_name=zone)
+
+
+    def dependency_cb(self, entity, attribute, old, new, kwargs):
+        eval_kwargs = {
+            'entity_id': entity,
+            'rule': kwargs.get('rule')
+        }
+        self.eval_rule(eval_kwargs)
+
+
+    def eval_rule(self, kwargs):
+        entity = kwargs.get('entity_id')
+        rule = kwargs.get('rule')
+        state = self.get_state(entity).lower()
+        rule_name = rule['name']
+        mode = rule['mode']
+        closed_sensors = self.rule_data[rule_name].get('closed_sensors', [])
+        open_sensors = self.rule_data[rule_name].get('open_sensors', [])
+        current_state = self.rule_data[rule_name]['state']
+        climate = self.args.get('climate')
+        new_state = None
+
+        # Add/remove the sensor from the closed_sensors or open_sensors list
+        if state == 'closed':
+            if entity not in closed_sensors:
+                self.rule_data[rule_name]['closed_sensors'].append(entity)
+            if entity in open_sensors:
+                self.rule_data[rule_name]['open_sensors'].remove(entity)
+        elif state == 'open':
+            if entity not in open_sensors:
+                self.rule_data[rule_name]['open_sensors'].append(entity)
+            if entity in closed_sensors:
+                self.rule_data[rule_name]['closed_sensors'].remove(entity)
+
+        # Get the open/closed count after manipulating the lists
+        open_count = len(self.rule_data[rule_name]['open_sensors'])
+        closed_count = len(self.rule_data[rule_name]['closed_sensors'])
+
+        # Set rule state once all sensors are accounted for
+        if (open_count + closed_count) == self.rule_data[rule_name]['total_sensors']:
+            if mode == 'any':
+                if open_count >= 1:
+                    new_state = 'active'
+                else:
+                    new_state = 'inactive'
+
+            elif mode == 'all':
+                if closed_count >= 1:
+                    new_state = 'inactive'
+                else:
+                    new_state = 'active'
+
+        # Add rule to rule counts
+        active_rules = self.rule_counts['active_rules']
+        inactive_rules = self.rule_counts['inactive_rules']
+        total_rules = self.rule_counts['total_rules']
+
+        # Add/remove the rule from the active/inactive lists
+        if new_state == 'inactive':
+            if entity not in inactive_rules:
+                self.rule_counts['inactive_rules'].append(entity)
+            if entity in active_rules:
+                self.rule_counts['active_rules'].remove(entity)
+        elif new_state == 'active':
+            if entity in inactive_rules:
+                self.rule_counts['inactive_rules'].remove(entity)
+            if entity not in active_rules:
+                self.rule_counts['active_rules'].append(entity)
+
+
+        # Take action based on rule state changes
+        if new_state == 'active' and new_state != current_state and len(self.rule_counts['active_rules']) >= 1:
+            # Only make changes if this is the first rule to fire (rules are 'any' not 'all')
+            self.log('{} (rule) is {}, turning off AC.'.format(rule_name, new_state))
+
+            # Store the new state and climate data
+            self.rule_data[rule_name]['state'] = new_state
+            self.climate_data['last_mode'] = self.get_state(climate, attribute='operation_mode')
+            self.climate_data['current_mode'] = 'off'
+
+            self.call_service(
+                service = 'climate/set_operation_mode',
+                entity_id = climate,
+                operation_mode = 'off'
+            )
+        elif new_state == 'inactive' and new_state != current_state and len(self.rule_counts['active_rules']) == 0:
+            # Only make changes if this is the last rule to turn off
+
+            self.rule_data[rule_name]['state'] = new_state
+            climate_mode = self.get_state(climate, attribute='operation_mode')
+
+            # Change AC back to the previous mode, but only if it hasn't been manually changed since it was turned off
+            if climate_mode == self.climate_data['current_mode'] and self.climate_data['last_mode']:
+                new_mode = self.climate_data['last_mode']
+                self.log('{} (rule) is {}, setting AC back to {}.'.format(rule_name, new_state, new_mode))
+                self.call_service(
+                    service = 'climate/set_operation_mode',
+                    entity_id = climate,
+                    operation_mode = new_mode
+                )
+                self.climate_data['last_mode'] = 'off'
+                self.climate_data['current_mode'] = new_mode
+        #self.log('{}, {}'.format(current_state, new_state))
+        #self.log(self.rule_data[rule_name])
