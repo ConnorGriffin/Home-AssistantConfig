71166d843a067593bf76d6803e4845b0555bea93
Connor Griffin <connor@connorcg.com>
2018-11-18 03:30:40 -0700

    Changed template sensor behavior, swapped priority for dependencies

--- appdaemon/apps/template_sensors/template_sensor.py
+++ appdaemon/apps/template_sensors/template_sensor.py
@@ -1,5 +1,6 @@
 import appdaemon.plugins.hass.hassapi as hass
 import re
+from time import sleep
 
 class TemplateSensor(hass.Hass):
 
@@ -25,7 +26,12 @@ class TemplateSensor(hass.Hass):
                 string = entity_id
             )
 
-            # Set the value immediately (on AppDaemon startup)
+            # Do nothing until the base sensor exists. Sleeps are bad for AppDaemon but IDK, this seems alright.
+            while not self.get_state(entity_id):
+                self.log('Waiting for {} to exist.'.format(entity_id))
+                sleep(5)
+
+            # Set the value on AppDaemon startup
             self.update_sensor(
                 entity = entity_id,
                 attribute = attribute,
@@ -33,7 +39,7 @@ class TemplateSensor(hass.Hass):
                 friendly_name = friendly_name
             )
 
-            # Listen for
+            # Listen for changes and update the values immediately
             self.listen_state(
                 cb = self.sensor_cb,
                 entity = entity_id,
@@ -53,8 +59,10 @@ class TemplateSensor(hass.Hass):
             friendly_name = kwargs.get('friendly_name', None)
         )
 
-
     def update_sensor(self, entity, attribute, sensor, **kwargs):
+        device_class = self.args.get('device_class', None)
+        icon = self.args.get('icon', None)
+
         # Get the new value if provided, otherwise get state
         new = kwargs.get('new', self.get_state(entity, attribute=attribute))
 
@@ -100,10 +108,10 @@ class TemplateSensor(hass.Hass):
         attributes = {
             'friendly_name': friendly_name
         }
-        if self.args['device_class']:
-            attributes.update({'device_class': self.args['device_class']})
-        if self.args['icon']:
-            attributes.update({'icon': self.args['icon']})
+        if device_class:
+            attributes.update({'device_class': device_class})
+        if icon:
+            attributes.update({'icon': icon})
 
         # Update the sensor
         self.set_state(
